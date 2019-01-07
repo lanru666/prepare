807b362226543d675889ec90d77ec8394d786045
17381792 <17381792@qq.com>
2019-01-07 16:56:28 +0800

    clientv3 watch revision

---
+++ etcd_usage/demo9/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"go.etcd.io/etcd/clientv3"
+	"time"
+)
+
+func main() {
+	// lease 实现锁自动过期
+	// op操作
+	// txn事务 if else then
+	
+	var (
+		config clientv3.Config
+		client *clientv3.Client
+		err    error
+	)
+	//客户端配置
+	config = clientv3.Config{
+		Endpoints:   []string{"127.0.0.1:2379"},
+		DialTimeout: 5 * time.Second,
+	}
+	//建立仓库
+	if client, err = clientv3.New(config); err != nil {
+		fmt.Println(err)
+		return
+	}
+	client = client;
+}
