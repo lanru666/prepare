cb2c46574f806e15ad24dba1fe44c943ce70d6fd
17381792 <17381792@qq.com>
2019-01-07 17:53:38 +0800

    clientv3 watch revision

--- etcd_usage/demo9/main.go
+++ etcd_usage/demo9/main.go
@@ -21,10 +21,17 @@ func main() {
 		Endpoints:   []string{"127.0.0.1:2379"},
 		DialTimeout: 5 * time.Second,
 	}
-	//建立仓库
+	//建立连接
 	if client, err = clientv3.New(config); err != nil {
 		fmt.Println(err)
 		return
 	}
 	client = client;
+	// 1 上锁(创建租约 自动续租 拿着租约去抢占一个key)
+	
+	// 2 处理业务
+	
+	// 3 释放锁(取消自动续租，释放租约)
+	
+	
 }
